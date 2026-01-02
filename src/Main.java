c4542698b028d68103a5c809cad66701dbbdd72c
aayush rana magar <realnaramagaraayush@gmail.com>
2026-01-02 20:24:34 +0545

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import leetcodeModule.twoSumProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        twoSumProblem tp = new twoSumProblem();
+
+
+    }
+}
